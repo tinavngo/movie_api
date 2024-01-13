098781c3a9e3a8d0bd8e0d63d8f338ad59c773de
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-01-12 22:41:38 -0800

    Initial commit

---
+++ js/server.js
@@ -0,0 +1,18 @@
+const url = require('url');
+let addr = 'http://localhost:8080/default.html?year=2017&month=february';
+let q = new URL(addr,  'http://localhost:8080');
+
+console.log(q.host); // returns 'localhost:8080'
+console.log(q.pathname); // returns '/default.html'
+console.log(q.search); // returns '?year=2017&month=february'
+
+let qdata = q.query; // returns an object: { year: 2017, month: 'february' }
+console.log(qdata.month); // returns 'february'
+const http = require('http');
+
+http.createServer((request, response) => {
+response.writeHead(200, {'Content-Type' : 'text/plain'});
+response.end('Hello Node!\n');
+}).listen(8080);
+
+console.log('My first Node test server is running on Port 8080.')
