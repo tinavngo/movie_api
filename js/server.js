c1fcb636065067ce985877bf92f37bdfc1c9cdef
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-01-12 23:17:03 -0800

    Removed unneeded URL modules

--- js/server.js
+++ js/server.js
@@ -1,14 +1,34 @@
-const url = require('url');
-let addr = 'http://localhost:8080/default.html?year=2017&month=february';
-let q = new URL(addr,  'http://localhost:8080');
 
-console.log(q.host); // returns 'localhost:8080'
-console.log(q.pathname); // returns '/default.html'
-console.log(q.search); // returns '?year=2017&month=february'
-
-let qdata = q.query; // returns an object: { year: 2017, month: 'february' }
-console.log(qdata.month); // returns 'february'
 const http = require('http');
+  fs = require('fs'),
+  url = require('url');
+
+//Variable declarations
+  http.createServer((request, response) => {
+    let addr = request.url,
+      q = new URL(addr, 'http://localhost:8080'),
+      filePath = '';
+
+      //if else statements for doc&html
+      if (q.pathname.includes('documentation')) {
+        filePath = (__dirname + '/documentation.html');
+      } else {
+        filePath = 'index.html';
+      }
+    
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          throw err;
+        }
+    
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+    
+      });
+    
+    }).listen(8080);
+    console.log('My test server is running on Port 8080.');
 
 http.createServer((request, response) => {
 response.writeHead(200, {'Content-Type' : 'text/plain'});
