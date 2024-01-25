a9a9a3fa51f5ba9d92be64cdf09a09acdcaa5d5f
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-01-25 09:34:38 -0800

    Added CRUD
    (post, get, put, delete)

--- index.js
+++ index.js
@@ -126,17 +126,125 @@ app.use(express.static('public'));
 // a ‘log.txt’ file is created in root directory
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
-// setup the logger
+// log request setup
 app.use(morgan('combined', {stream: accessLogStream}));
 
-// GET requests
+//CREATE
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  }else{
+    res.status(400).send('User need names')
+  }
+})
+
+//UPDATE
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+  
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('no such user')
+  }
+})
+
+//CREATE
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+  
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user')
+  }
+})
+
+//DELETE
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+  
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle); //strict equality, if not equal to, then do not show/remove
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send('no such user')
+  }
+})
+
+
+//DELETE
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    users = users.filter( user => user.id != id); // non strict equality because string and number
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send('no such user')
+  }
+})
+
+// READ aka GET requests
 app.get("/", (req, res) => {
   let responseText = "Welcome to myFlix app!";
   res.send(responseText);
 });
 
+//READ
 app.get("/movies", (req, res) => {
-  res.json(moviesList);
+  res.status(200).json(movies);
+});
+
+//READ
+app.get("/movies/:title", (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find( movie => movie.Title === title );
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('no such movie')
+  }
+});
+
+//READ
+app.get("/movies/genre/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('no such genre')
+  }
+});
+
+//READ
+app.get("/movies/director/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movie.Director.Name === directorName ).Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('no such director')
+  }
 });
 
 //error handling middleware function
