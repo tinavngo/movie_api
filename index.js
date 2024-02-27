39fa169e13af5c674fccc3abeac5783c500a9423
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-02-27 11:52:39 -0800

    Added Express get users endpoint

--- index.js
+++ index.js
@@ -226,6 +226,33 @@ async (req, res) => {
      })
 });
 
+// READ all users
+app.get('/users', 
+passport.authenticate('jwt', { session: false }), 
+async (req, res) => {
+  await Users.find()
+      .then((users) => {
+          res.status(201).json(users);
+      })
+      .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+      });
+});
+
+// READ a user by username
+app.get('/users/:Username', 
+passport.authenticate('jwt', { session: false }), 
+async (req, res) => {
+  await Users.findOne({ Username: req.params.Username })
+      .then((user) => {
+          res.json(user);
+      })
+      .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+      });
+});
 
 //CREATE Add a movie to a user's list of favorites must auth x
 app.post(
