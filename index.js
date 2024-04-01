1b648319726ba670beb4835321ad49bf8c168e67
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-04-01 12:07:49 -0700

    Add netifly in CORS

--- index.js
+++ index.js
@@ -29,10 +29,32 @@ mongoose.connect(process.env.CONNECTION_URI, {
   useUnifiedTopology: true,
 });
 
- // cross origin source
- const cors = require('cors');
- app.use(cors());
- const {check, validationResult } = require('express-validator');
+/**
+ * Create web server
+ */
+app = express();
+
+/**
+ * Middleware to parse incoming request bodies as JSON
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * Cross Origin Resource Sharing
+ */
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://cinematflix.netlify.app']
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 
 /**
