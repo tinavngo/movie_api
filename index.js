2328da3a220a0abe62b1c8c62d96404a03df8ee6
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-04-24 14:53:47 -0700

    Remove extra updatedUser checks

--- index.js
+++ index.js
@@ -182,22 +182,23 @@ app.put(
   '/users/:Username',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
-  [
-    // Validation logic here for request
-    check('Username', 'Username is required').isLength({ min: 5 }),
-    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-    check('Password', 'Password is required').not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isEmail()
-  ],
+    [
+      // Validation logic here for request
+      check('Username', 'Username is required').isLength({ min: 5 }),
+      /* check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+       check('Password', 'Password is required').not().isEmpty(),
+       check('Email', 'Email does not appear to be valid').isEmail()*/
+    ],
 
-   (req, res) => {
-    //CONDITION TO CHECK ADDED HERE
-    if (req.user.Username !== req.params.Username) {
-      return res.status(400).send('Permission denied');
-    }
+      (req, res) => {
+        // check the validation object for errors
+        let errors = validationResult(req);
 
-  }
-
+        if (!errors.isEmpty()) {
+          return res.status(422).json({ errors: errors.array() });
+        }
+      }
+      
     let hashedPassword = Users.hashPassword(req.body.Password);
      Users.findOneAndUpdate({ Username: req.params.Username }, {
       $set:
